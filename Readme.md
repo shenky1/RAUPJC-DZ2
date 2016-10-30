b6618f5cb83f7315231ab3e99c52d66d5f948eaf
Matija <matijaselendic@gmail.com>
2016-10-30 20:15:10 +0100

    Zadatak 6

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Pitanje 1:
+Izvršavanje programa trajalo je 5.0011261 sekundi.
+
+# Pitanje 2:
+Sve operacije izvršavale su se na istoj dretvi.
+
+# Pitanje 3:
+Sada je izvršavanje programa trajalo 1.0219756 sekundi.
+
+# Pitanje 4:
+Operacije su se izvršavale svaka na svojoj dretvi. 5 ukupno.
+
+# Pitanje 5:
+Paralelna petlja trajala je 0.5826 milisekundi, a obièna 0.0041 milisekundi.
---
+++ Zadatak6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zadatak6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object _lock = new object();
+
+            List<int> results = new List<int>();
+            Parallel.For(0, 100, (i) =>
+            {
+                Thread.Sleep(1);
+                lock (_lock)
+                {
+                    results.Add(i * i);
+                }
+            });
+            Console.WriteLine("Bag length should be 100. Length is {0}", results.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
